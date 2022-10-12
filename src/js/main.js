6f6ed77b51cccfcd53da3543f092c9583fcdadb5
LeoZion <leozionsuporte@gmail.com>
2022-10-12 16:26:31 -0300

    multiple requests

--- src/js/main.js
+++ src/js/main.js
@@ -47,7 +47,13 @@ const del = () => {
 }
 
 const multiple = () => {
-    console.log('multiple');
+    Promise.all([
+        axios.get('https://jsonplaceholder.typicode.com/posts'),
+        axios.get('https://jsonplaceholder.typicode.com/users')
+    ]).then((response) => {
+        console.table(response[0].data);
+        console.table(response[1].data);
+    })
 }
 
 const transform = () => {
